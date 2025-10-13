d5b36d489ba831fbe37b97859f1ef8ddbd8c7f54
andreimoreira <dedeigalinha@hotmail.com>
2025-10-13 10:44:21 -0300

    feat: model Avaliacao and Endereco created

--- aplicativoEcommerce/EcommerceAPI/app/Models/Avaliacao.php
+++ aplicativoEcommerce/EcommerceAPI/app/Models/Avaliacao.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Avaliacao extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'avaliacao';
+    private $timestamp = false;
+    protected $fillable = [
+        'usuario_id',
+        'produto_id',
+        'nota',
+        'comentario',
+    ];
+
+    public function produto()
+    {
+        return $this->belongsTo(Produto::class);
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class);
+    }
 }
--- aplicativoEcommerce/EcommerceAPI/app/Models/Endereco.php
+++ aplicativoEcommerce/EcommerceAPI/app/Models/Endereco.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Endereco extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'endereco';
+
+    private $timestamp=false;
+
+    protected $fillable = [
+        'usuario_id',
+        'rua',
+        'numero',
+        'bairro',
+        'cidade',
+        'estado',
+        'cep',
+        'complemento',
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class);
+    }
 }
