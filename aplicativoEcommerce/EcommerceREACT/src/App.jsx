377141a71df312707cf990fb6a8ae28ceb48896b
andreimoreira <dedeigalinha@hotmail.com>
2025-10-20 09:18:04 -0300

    feat: new pages under construction and new functionalities added

--- aplicativoEcommerce/EcommerceREACT/src/App.jsx
+++ aplicativoEcommerce/EcommerceREACT/src/App.jsx
@@ -1,17 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/navBar/navBar'
-import Pedidos from './components/Pedidos/pedidos'
+import Home from './pages/Home'
+import Carrinho from './pages/carrinho'
+import Login from './pages/Login'
+import { PedidosProvider } from './context/pedidosContext'
 
 function App() {
 
   return (
-    <>
-      <div>
-        <NavBar/>
-        <Pedidos/>
-      </div>
-        
-    </>
+    <BrowserRouter>
+      <PedidosProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </PedidosProvider>
+    </BrowserRouter>
   )
 }
 
---
+++ aplicativoEcommerce/EcommerceREACT/src/context/pedidosContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useEffect } from "react";
+import { getPedidos } from '../api/pedidos';
+
+export const PedidoContext = createContext();
+
+export function PedidosProvider({children}) {
+    const [pedidos, setPedidos] = useState([]);
+    const [erro, setErro] = useState(null);
+
+    useEffect(() => {
+        async function carregarPedidos() {
+            try {
+                const data = await getPedidos();
+                setPedidos(data);
+            } catch {
+                setErro("Não foi possível carregar os pedidos.");
+            }
+        }
+
+        carregarPedidos();
+    }, []);
+    return (
+        <PedidoContext.Provider value={{pedidos, setPedidos, erro}}>
+            {children}
+        </PedidoContext.Provider>
+    )
+}
