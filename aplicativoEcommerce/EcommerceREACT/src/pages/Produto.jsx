a810244ae4f27c989ed55316ec485480da67c245
andreimoreira <dedeigalinha@hotmail.com>
2025-11-05 11:11:39 -0300

    layout de produto por id completo

--- aplicativoEcommerce/EcommerceREACT/src/pages/Produto.jsx
+++ aplicativoEcommerce/EcommerceREACT/src/pages/Produto.jsx
@@ -1,22 +1,40 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProdutoContext } from "../context/produtoContext";
+import { useParams } from "react-router-dom";
+import "./produto.css"
 
 export default function Produto() {
     const { produtos, erro } = useContext(ProdutoContext);
+    const { id } = useParams()
+    const [coracao, setCoracao] = useState(false)
+    if (erro) return <p>{erro}</p>
+    if (!produtos.length) return <p>Carregando...</p>
+
+    const produto = produtos.find(p => p.id == id)
+
+    if (!produto) return <p>Produto não encontrado.</p>
 
     return (
-        <div>
-            <img src="" alt="" />
+        <>
+            <div className="div-pai-produtos">
+                <img src={produto.imagem} alt="" />
 
-            <div>
-                <i class="fa-solid fa-cart-plus"></i>
-                <h2>Titulo do produto</h2>
-                <p>Descição do produto</p>
-                <h1>preço do produto</h1>
-                <button>Compre Agora</button>
-                <i className="fa-regular fa-heart"></i>
-
+                <div >
+                    <div className="titulo-icone">
+                        <div className="div-filho-produtos">
+                            <i onClick={() => setCoracao(!coracao)} className={`icone-fav ${coracao ? "fa-solid" : "fa-regular"} fa-heart`}
+                            ></i></div>
+                        <h1 className="titulo-produto">{produto.nome}</h1>
+                    </div>
+                    <div className="preco-produto"><p className="h2-preco-produto">R${produto.preco}</p></div>
+                    <div className="div-botoes">
+                        <button className="botao-comprar">Compre Agora</button>
+                        <button className="botao-adicionar-carrinho">Adicionar Ao Carrinho<i class="fa-solid fa-cart-plus"></i></button>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className="paragrafo-produto"><p>{produto.descricao}</p></div>
+            
+        </>
     )
 }
