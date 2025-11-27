f9e35047da4eecf4ed2555ed402abc84cd811f53
andreimoreira <dedeigalinha@hotmail.com>
2025-11-27 11:09:52 -0300

    metodos e funções implementadas com fetch de itens carrinho.

--- aplicativoEcommerce/EcommerceREACT/src/api/itensCarrinho.js
+++ aplicativoEcommerce/EcommerceREACT/src/api/itensCarrinho.js
@@ -1,13 +1,46 @@
 import { API_ItensCarrinhos } from "./config";
 
-export async function deleteItem(itemId, token) {
-    const resposta = await fetch(`${API_ItensCarrinhos}/${itemId}`, {
-        method: 'DELETE',
+async function fetchComToken(url, options = {}, token) {
+    const resposta = await fetch(url, {
+        ...options, 
         headers: {
-            Authorization: `Bearer ${token}`
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            ...(options.headers || {})
         }
     });
 
-    if (!resposta.ok) throw new Error("Erro ao remover");
-    return (await resposta).json();
+    if (!resposta.ok) throw new Error("Erro na requisição");
+    return await resposta.json();
+}
+
+export async function getItensCarrinho(token) {
+    return fetchComToken(API_ItensCarrinhos, {}, token)
+}
+
+export async function showItemCarrinho(itemId, token) {
+    return fetchComToken(`${API_ItensCarrinhos}/${itemId}`, {}, token)
+}
+
+export async function addCarrinho(produtoId, token) {
+    return fetchComToken(API_ItensCarrinhos, {
+        method: 'POST',
+        body: JSON.stringify({
+            produto_id: produtoId,
+            quantidade: 1
+        })
+    }, token)
+}
+
+export async function deleteItem(itemId, token) {
+    return fetchComToken(`${API_ItensCarrinhos}/${itemId}`, {
+        method: 'DELETE',
+    }, token)
+}
+
+export async function updateItem(itemId, quantidade, token) {
+    return fetchComToken(`${API_ItensCarrinhos}/${itemId}`, {
+        method: 'PUT',
+        body: JSON.stringify({ quantidade })
+    }, token)
 }
